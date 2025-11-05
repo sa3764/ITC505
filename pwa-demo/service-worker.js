b95f2d8ce59cccd0697cf2ae1b6d3aed2170ced0
Sadhana Anumasu <sa3764@nau.edu>
2025-11-04 23:03:25 -0700

    Add files via upload

---
+++ pwa-demo/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "todoistic-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
